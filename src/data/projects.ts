9bcd354aa30caf890ad02e49bde4540df9c89836
liam <39243060+devldm@users.noreply.github.com>
2023-10-08 18:14:13 +0900

    adding korean lang support

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,7 @@ export type ProjectType = {
   imgPath?: string;
   title: string;
   body: string;
+  body_kr?: string;
   stack: string[];
   liveHref: string | null;
   github: string;
@@ -9,9 +10,10 @@ export type ProjectType = {
 
 export const Projects: ProjectType[] = [
   {
-    imgPath: "images/gc.png",
+    imgPath: "/images/gc.png",
     title: "GrammarCheck",
     body: "In progress project that allows people to practice Korean grammar.",
+    body_kr: "한국어 문법 연습할 수 있습니다.",
     liveHref: "",
     stack: [
       "React",
@@ -24,15 +26,16 @@ export const Projects: ProjectType[] = [
     github: "https://github.com/devldm/grammar-check",
   },
   {
-    imgPath: "images/airbnb-crop-more-1.png",
+    imgPath: "/images/airbnb-crop-more-1.png",
     title: "airbnb remake",
     body: "Remake of airbnb website where you can view listings",
+    body_kr: "airbnb 비슷한 사이트. 다른 airbnb의 볼 수 있습니다.",
     liveHref: "https://go-next-three.vercel.app/",
     stack: ["React", "tailwindCSS", "Next.js", "Go", "Fiber", "MongoDB"],
     github: "https://github.com/devldm/go-next",
   },
   {
-    imgPath: "images/wallet-crop-1.png",
+    imgPath: "/images/wallet-crop-1.png",
     title: "Simple Android Wallet",
     body: "lightweight and android native cryptocurrency wallet. Send, receive and store currencies held on ethereum networks",
     liveHref: "",
@@ -46,9 +49,11 @@ export const Projects: ProjectType[] = [
     github: "https://github.com/devldm/SimpleAndroidWallet",
   },
   {
-    imgPath: "images/go-skmore-crop.png",
+    imgPath: "/images/go-skmore-crop.png",
     title: "GO-SK",
     body: "I am currently looking for a role in South Korea and wanted to help others who might be in my position by making a platform suited for them.",
+    body_kr:
+      "지금은 한국 회사가 취직하고 싶어서 한국 채용 사이트가 만들었습니다.",
     liveHref: "https://go-sk.vercel.app/",
     stack: [
       "React",
